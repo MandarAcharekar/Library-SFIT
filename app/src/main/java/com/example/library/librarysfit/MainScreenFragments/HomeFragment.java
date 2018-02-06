02c94cc0da843aefbe27bd37bae7f8718de98244
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-06 17:28:59 +0530

    Add LoginButton

--- app/src/main/java/com/example/library/librarysfit/MainScreenFragments/HomeFragment.java
+++ app/src/main/java/com/example/library/librarysfit/MainScreenFragments/HomeFragment.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.library.librarysfit.R;
@@ -17,6 +18,8 @@ import com.example.library.librarysfit.R;
 
 public class HomeFragment extends Fragment {
 
+  Button homeLogin;
+
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState){
@@ -25,6 +28,19 @@ public class HomeFragment extends Fragment {
     View view = inflater.inflate(R.layout.page_home, container, false);
 
     view.setBackgroundColor(Color.RED);
+
+    homeLogin = view.findViewById(R.id.btn_login_home);
+    homeLogin.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        Toast.makeText(getActivity(), "button Press",
+                Toast.LENGTH_SHORT).show();
+
+        
+      }
+    });
+
+
     return view;
   }
 
