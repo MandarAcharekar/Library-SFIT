46f0b891a9aa220b9f3ddd35667c657ba1fd3f24
RishabhBhatnagar <bhatnagarrishabh4@gmail.com>
2018-02-08 20:54:06 +0530

    Test Button added in home Fragment

--- app/src/main/java/com/example/library/librarysfit/MainScreenFragments/HomeFragment.java
+++ app/src/main/java/com/example/library/librarysfit/MainScreenFragments/HomeFragment.java
@@ -1,21 +1,25 @@
 package com.example.library.librarysfit.MainScreenFragments;
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.library.librarysfit.Cards;
 import com.example.library.librarysfit.R;
 
-/**
- * Created by vinay on 04-02-2018.
- */
-
 public class HomeFragment extends Fragment {
+
+
+  private Button recycle_button;
+
+
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -23,6 +27,26 @@ public class HomeFragment extends Fragment {
     // Inflate the layout for this fragment
 
     View view = inflater.inflate(R.layout.page_home, container, false);
+
+
+
+
+
+
+    //Adding a click listener to test the Card Java and xml components.
+    recycle_button = view.findViewById(R.id.button_recyclerview);
+    recycle_button.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent recyclerCards = new Intent(getActivity(), Cards.class);
+            startActivity(recyclerCards);
+
+        }
+    });
+
+
+
 
     view.setBackgroundColor(Color.RED);
     return view;
