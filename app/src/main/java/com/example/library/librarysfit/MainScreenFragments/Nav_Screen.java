6c8a7a4b6a621b4e0a203b3aaca393dfce3eed54
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-18 21:26:19 +0530

    Created new activity

---
+++ app/src/main/java/com/example/library/librarysfit/MainScreenFragments/Nav_Screen.java
@@ -0,0 +1,14 @@
+package com.example.library.librarysfit.MainScreenFragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.library.librarysfit.R;
+
+public class Nav_Screen extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_nav__screen);
+  }
+}
