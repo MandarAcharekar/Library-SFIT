47dfa76709503767554082aa6c3d23142e6b4672
vinay-deshmukh <vinay_deshmukh@outlook.com>
2018-02-18 21:15:14 +0530

    jSoup - First Commit

--- app/src/main/java/com/example/library/librarysfit/MainScreenFragments/NavFragment.java
+++ app/src/main/java/com/example/library/librarysfit/MainScreenFragments/NavFragment.java
@@ -18,7 +18,7 @@ public class NavFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState){
     // Inflate the layout for this fragment
-    View view = inflater.inflate(R.layout.page_navigation, container, false);
+    View view = inflater.inflate(R.layout.page_navigation_temp, container, false);
     return view;
   }
 }
